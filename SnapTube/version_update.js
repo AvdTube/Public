0c73c6fbcec5ccc664efce8c35987b5a7d06f632
AvdTube <ooemio@sina.com>
2018-06-14 15:50:47 +0800

    Update version_update.js

--- SnapTube/version_update.js
+++ SnapTube/version_update.js
@@ -1,1 +1,7 @@
-{"versionCode":"1","versionName":"1.0","updateMsg":"1.VersionCheckLib V2版震撼来袭\n2.链式编程，调用简单\n3.强大的自定义功能，一行代码轻松搞定\n4.欢迎Star和Issue\n5.github:<a href='http://github.com/alexliusheng'>http://github.com/alexliusheng</a>","versionApkUrl":"true","updateTitle":"New Version Detected"}
+{
+	"versionCode": "1",
+	"versionName": "1.0",
+	"updateMsg": "1.VersionCheckLib V2版震撼来袭\n2.链式编程，调用简单\n3.强大的自定义功能，一行代码轻松搞定\n4.欢迎Star和Issue\n5.github:<a href='http://github.com/alexliusheng'>http://github.com/alexliusheng</a>",
+	"versionApkUrl": "https://github.com/AvdTube/Public/raw/master/SnapTube/apk/snaptube.apk",
+	"updateTitle": "New Version Detected"
+}
